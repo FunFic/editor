52b53a90867fd309f845e8398555c435e7a3f56a
Vamoss <quissama@gmail.com>
2021-06-30 08:56:39 -0300

    atualiza o service worker

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,12 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v1.0.0';
+var version;// Change number to update the cache
+version = "v1.0.0";//beta
+version = "v1.0.1";//fix typing and focus conflict
+const PRECACHE = 'precache-'+version;
 const RUNTIME = 'runtime';
 
-// A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   './index.html',
-  './',// Alias for index.html
+  './',
   './css/bootstrap/bootstrap.min.css',
   './css/styles.css',
   './js/ace/ace.js',
@@ -49,7 +35,6 @@ const PRECACHE_URLS = [
   './images/icones/down.png'
 ];
 
-// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(PRECACHE)
@@ -58,7 +43,6 @@ self.addEventListener('install', event => {
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
   const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
@@ -72,11 +56,7 @@ self.addEventListener('activate', event => {
   );
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
 self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
@@ -86,7 +66,6 @@ self.addEventListener('fetch', event => {
 
         return caches.open(RUNTIME).then(cache => {
           return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
             return cache.put(event.request, response.clone()).then(() => {
               return response;
             });
