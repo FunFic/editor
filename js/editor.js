d9d06cfec981f50faacbaa1d8cae9c7d3a86001f
Vamoss <quissama@gmail.com>
2021-07-01 16:45:36 -0300

    separa o script de instalação do ServiceWorker

--- js/editor.js
+++ js/editor.js
@@ -1166,20 +1166,4 @@ const iframeHtml = `<!DOCTYPE html>
     });
   }
 //#endregion
-
-
-//#region Service Worker
-  if (!localhost){
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('service-worker.js')
-      .then(function(registration) {
-        // Registration was successful
-        // console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }).catch(function(err) {
-        // registration failed :(
-        // console.log('ServiceWorker registration failed: ', err);
-      });
-    }
-  }
-//#endregion
 });//DOMContentLoaded
